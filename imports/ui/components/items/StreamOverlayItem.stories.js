97d0a43b703ec43bd0d83be8247f62395c92388f
MarlonWelter <mrmwelter@gmail.com>
2018-12-27 11:20:48 +0100

    feat(stories): add item stories (#3)

---
+++ imports/ui/components/items/StreamOverlayItem.stories.js
@@ -0,0 +1,5 @@
+import StreamOverlayItem from './StreamOverlayItem';
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+storiesOf('StreamOverlayItem', module).add('StreamOverlayItem', () => <StreamOverlayItem />);
