268095012aae7a5c26db40ad7d1d4027b7f56470
Leon Machens <leon.machens@grapes.de>
2018-12-30 16:35:51 +0100

    feat(champions): add range field

---
+++ imports/api/champions/server/getRange.js
@@ -0,0 +1,14 @@
+import RANGE from '../range';
+
+const shapeshifters = ['Elise', 'Gnar', 'Kayle'];
+const getRange = ({ id, stats }) => {
+  if (shapeshifters.includes(id)) {
+    return RANGE.SHAPE_SHIFTER;
+  }
+  if (stats.attackrange < 300) {
+    return RANGE.MELEE;
+  }
+  return RANGE.RANGED;
+};
+
+export default getRange;
