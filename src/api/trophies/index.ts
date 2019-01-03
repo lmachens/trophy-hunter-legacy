d9be8dbc1ca6bdf55f19644e87d6d1152af37b32
Leon Machens <leon.machens@googlemail.com>
2019-01-03 22:24:42 +0100

    remove unused deps

--- src/api/trophies/index.ts
+++ src/api/trophies/index.ts
@@ -4,19 +4,8 @@ import extendMatchResult from '../../shared/matches/extendMatchResult';
 import { calculateTrophies } from '../../shared/trophies/calculateTrophies';
 import { parse } from 'url';
 
-export default (req: IncomingMessage, res: ServerResponse) => {
-  const { region, matchId, summonerId }: any = parse(req.url, true).query;
-  if (!region) {
-    return res.end('No Region');
-  }
-  if (!matchId) {
-    return res.end('No MatchID');
-  }
-  if (!summonerId) {
-    return res.end('No SummonerID');
-  }
-
-  const matchPromise = new Promise((resolve, reject) => {
+const getMatch = ({ region, matchId }) => {
+  return new Promise((resolve, reject) => {
     get(
       `https://${region}.api.riotgames.com/lol/match/v3/matches/${matchId}?api_key=${
         process.env.LEAGUE_API_KEY
@@ -38,8 +27,10 @@ export default (req: IncomingMessage, res: ServerResponse) => {
       reject(err.message);
     });
   });
+};
 
-  const timelinePromise = new Promise((resolve, reject) => {
+const getTimeline = ({ region, matchId }) => {
+  return new Promise((resolve, reject) => {
     get(
       `https://${region}.api.riotgames.com/lol/match/v3/timelines/by-match/${matchId}?api_key=${
         process.env.LEAGUE_API_KEY
@@ -61,13 +52,33 @@ export default (req: IncomingMessage, res: ServerResponse) => {
       reject(err.message);
     });
   });
+};
+
+export default (req: IncomingMessage, res: ServerResponse) => {
+  const { region, matchId, summonerId }: any = parse(req.url, true).query;
+  if (!region) {
+    return res.end('No Region');
+  }
+  if (!matchId) {
+    return res.end('No MatchID');
+  }
+  if (!summonerId) {
+    return res.end('No SummonerID');
+  }
+
+  const matchPromise = getMatch({ region, matchId });
+  const timelinePromise = getTimeline({ region, matchId });
 
   Promise.all([matchPromise, timelinePromise])
     .then(([match, timeline]: [any, any]) => {
       match.timeline = timeline;
       const extendedMatchResult = extendMatchResult(null, match, parseInt(summonerId), null);
       const trophiesObtained = calculateTrophies({ extendedMatchResult });
-      res.end(JSON.stringify(trophiesObtained));
+      const result = {
+        data: trophiesObtained.map(trophyObtained => trophyObtained.trophy),
+        count: trophiesObtained.length
+      };
+      res.end(JSON.stringify(result));
     })
     .catch(errorMessage => {
       res.end(errorMessage);
