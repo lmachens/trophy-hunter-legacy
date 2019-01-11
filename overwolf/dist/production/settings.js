f5e2d06908f6431d0c0b56b9c68dabf0ec000d48
Leon Machens <leon.machens@googlemail.com>
2019-01-11 17:59:22 +0100

    update url

--- overwolf/dist/production/settings.js
+++ overwolf/dist/production/settings.js
@@ -1,1 +1,1 @@
-const settings={name:'Production',domain:'https://app.loltrophyhunter.com',overlayDomain:'https://app.loltrophyhunter.com/overlay',overlayHotkey:'trophy_hunter_overlay'};export default settings;
+const settings={name:'Production',domain:'https://app.loltrophyhunter.com',overlayDomain:'https://app.loltrophyhunter.com/live',overlayHotkey:'trophy_hunter_overlay'};export default settings;
