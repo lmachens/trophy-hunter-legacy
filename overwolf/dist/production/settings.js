14cab7441a68c715f3cf31152d78e96b60c98c09
Leon Machens <leon.machens@googlemail.com>
2019-01-12 11:00:36 +0100

    chore: move overwolf src

--- overwolf/dist/production/settings.js
+++
@@ -1,1 +0,0 @@
-const settings={name:'Production',domain:'https://app.loltrophyhunter.com',overlayDomain:'https://app.loltrophyhunter.com/live',overlayHotkey:'trophy_hunter_overlay'};export default settings;
