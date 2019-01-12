29eb61b033fe70a83fb3500a0f12faa82ea0b80e
Leon Machens <leon.machens@googlemail.com>
2019-01-12 12:05:17 +0100

    fix(api): stop caching active match

--- src/api/shared/th-api/getActiveGame.ts
+++ src/api/shared/th-api/getActiveGame.ts
@@ -1,28 +1,14 @@
 import axios from 'axios';
-import NodeCache from 'node-cache';
 
 const apiEndpoint = process.env.ACTIVE_GAME_API_ENDPOINT || 'https://www.th.gl/api/active-game';
 
-const cache = new NodeCache({
-  checkperiod: 120, // seconds
-  stdTTL: 100 // seconds
-});
-
 const getActiveGame = ({ platformId, summonerId }) => {
-  const key = `${platformId}&${summonerId}`;
-  const data = cache.get(key);
-  if (data) {
-    return new Promise(resolve => resolve(data));
-  }
   const version = typeof summonerId === 'number' ? 'v3' : 'v4';
   const url = `${apiEndpoint}?platformId=${platformId}&summonerId=${summonerId}&version=${version}`;
 
   return axios
     .get(url)
     .then(response => {
-      if (response.data) {
-        cache.set(key, response.data);
-      }
       return response.data;
     })
     .catch(error => {
