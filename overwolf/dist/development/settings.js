f7806fbcae4dca75ffc1c596ece068c363346eb5
Leon Machens <leon.machens@googlemail.com>
2019-01-12 11:01:51 +0100

    chore: move overwolf src (#67)

--- overwolf/dist/development/settings.js
+++
@@ -1,8 +0,0 @@
-const settings = {
-  name: 'Development',
-  domain: 'http://localhost:3000',
-  overlayDomain: 'http://localhost:3000/live',
-  overlayHotkey: 'trophy_hunter_overlay_dev'
-};
-
-export default settings;
