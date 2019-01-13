3e4d2d2a1e582a7ee72ea1ba6f7991b09e216249
Leon Machens <leon.machens@googlemail.com>
2019-01-13 15:01:50 +0100

    finalize

--- src/creator/components/LoadMatch.tsx
+++ src/creator/components/LoadMatch.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { getMatch } from '../shared/th-api';
+import { getMatchWithTimeline } from '../shared/th-api';
 
 const LoadMatch = ({ onLoad }) => {
   const handleClick = () => {
-    getMatch({ platformId: 'euw1', matchId: 3795861223 }).then(onLoad);
+    getMatchWithTimeline({ platformId: 'euw1', matchId: 3795861223 }).then(onLoad);
   };
 
   return <button onClick={handleClick}>Load</button>;
--- src/creator/components/Match.tsx
+++ src/creator/components/Match.tsx
@@ -3,29 +3,69 @@ import dynamic from 'next/dynamic';
 import { JSONEditorProps } from './types';
 import LoadMatch from './LoadMatch';
 import extendMatchResult from '../shared/matches/extendMatchResult';
+import calculateTrophies from '../shared/trophies/calculateTrophies';
 
 const DynamicJSONEditor = dynamic<JSONEditorProps>(import('./JSONEditor') as any, { ssr: false });
 
+const matchKey = 'th-creator-match';
+let lastMatch;
+try {
+  const storedMatch = typeof localStorage === 'object' && localStorage.getItem(matchKey);
+  lastMatch = JSON.parse(storedMatch);
+} catch (error) {
+  lastMatch = 'Load a match first';
+}
+const handleChangeText = text => {
+  console.log(text);
+  if (typeof localStorage === 'object') {
+    localStorage.setItem(matchKey, text);
+  }
+};
 const Match = () => {
-  const [match, setMatch] = useState({});
-  const [extendedMatch, setExtendedMatch] = useState({ a: 2 });
+  const [match, setMatch] = useState(lastMatch);
+  const [participants, setParticipants] = useState('Loading');
 
   useEffect(() => {}, []);
 
   const handleValidate = json => {
     try {
-      const extendedMatchResult = extendMatchResult({ matchResult: match });
-      //setExtendedMatch(extendedMatchResult);
-    } catch (error) {}
-    setExtendedMatch(json);
+      const result = json.participantIdentities.reduce((result, participantIdentity) => {
+        const extendedMatchResult = extendMatchResult({
+          matchResult: json,
+          summonerName: participantIdentity.player.summonerName
+        });
+        const stats = extendedMatchResult.participant.stats;
+        const trophies = calculateTrophies({ extendedMatchResult });
+        result[participantIdentity.player.summonerName] = {
+          stats,
+          trophies
+        };
+        return result;
+      }, {});
+      setParticipants(result);
+    } catch (error) {
+      console.error(error);
+      setParticipants(error.message);
+    }
+
     return [];
+  };
+
+  const handleLoad = value => {
+    setMatch(value);
+    handleChangeText(value);
   };
 
   return (
     <div>
-      <LoadMatch onLoad={setMatch} />
-      <DynamicJSONEditor json={match} onValidate={handleValidate} mode="code" />
-      <DynamicJSONEditor json={extendedMatch} mode="tree" />
+      <LoadMatch onLoad={handleLoad} />
+      <DynamicJSONEditor
+        json={match}
+        onValidate={handleValidate}
+        mode="code"
+        onChangeText={handleChangeText}
+      />
+      <DynamicJSONEditor json={participants} mode="tree" />
     </div>
   );
 };
