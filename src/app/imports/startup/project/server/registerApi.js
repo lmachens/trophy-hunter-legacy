2d129786cf052008668c1eedc7750efa80d2b41a
Leon Machens <leon.machens@googlemail.com>
2019-01-19 18:46:59 +0100

    remove unused files

--- src/app/imports/startup/project/server/registerApi.js
+++
@@ -1,8 +0,0 @@
-import '/imports/api/timestampedCollection';
-import '/imports/api/game-sessions/gameSessions';
-import '/imports/api/game-sessions/server/publications';
-import '/imports/api/trophies/server/methods';
-import '/imports/api/trophy-hunters/trophyHunters';
-import '/imports/api/trophy-hunters/server/methods';
-import '/imports/api/trophy-hunters/server/publications';
-import '/imports/api/riot-api/server/methods';
--- src/app/imports/ui/layouts/ProjectLayout.stories.js
+++
@@ -1,5 +0,0 @@
-import ProjectLayout from './ProjectLayout';
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-storiesOf('Layouts', module).add('ProjectLayout', () => <ProjectLayout />);
