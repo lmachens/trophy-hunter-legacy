ca4be1a105a60db0f830eedce0ad540a38fe5ad9
Leon Machens <leon.machens@googlemail.com>
2019-02-06 21:51:58 +0100

    Revert "fix: @material-ui/styles install usage was incorrect (#123)" (#124)
    This reverts commit 611d8d85620ae3909292bfab06f469a71a732adc.

--- src/www/pages/_bootstrap.ts
+++ src/www/pages/_bootstrap.ts
@@ -1,3 +1,4 @@
 // https://material-ui.com/css-in-js/basics/#migration-for-material-ui-core-users
-const install = require('@material-ui/styles').install;
+import { install } from '@material-ui/styles';
+
 install();
