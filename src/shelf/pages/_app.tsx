1d8d7b4b18a297196c8c6c2e7e5853b93561df0a
Leon Machens <leon.machens@googlemail.com>
2019-02-06 21:51:45 +0100

    Revert "fix: @material-ui/styles install usage was incorrect (#123)"
    This reverts commit 611d8d85620ae3909292bfab06f469a71a732adc.

--- src/shelf/pages/_app.tsx
+++ src/shelf/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@material-ui/styles';
 import App, { Container } from 'next/app';
 import React from 'react';
+import './_bootstrap';
 import theme from './_theme';
 
 export default class MyApp extends App<any> {
--- src/www/pages/_app.tsx
+++ src/www/pages/_app.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from '@material-ui/styles';
 import App, { Container } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
+import './_bootstrap';
 import theme from './_theme';
 
 export default class MyApp extends App<any> {
